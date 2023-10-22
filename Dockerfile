73d13ce2ae2d5524ccd8a5505ed71f4d2b3b78f1
krichen-Noureddine <noureddine.krichen@esprit.tn>
2023-10-23 00:25:02 +0100

    update config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
-EXPOSE 8082
+EXPOSE 8089
 ADD target/review-0.0.1-SNAPSHOT.jar ReviewMS-docker.jar
 ENTRYPOINT ["java", "-jar", "ReviewMS-docker.jar"]
--- src/main/java/com/ecommerce/review/ReviewMsApplication.java
+++ src/main/java/com/ecommerce/review/ReviewMsApplication.java
@@ -2,10 +2,11 @@ package com.ecommerce.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+
 public class ReviewMsApplication {
 
 
